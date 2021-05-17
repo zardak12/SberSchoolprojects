1b401959f334c5077da1dd05b44f7a5680628d6c
mark <shmarkyr@gmail.com>
2021-05-17 23:56:32 +0300

    commit

---
+++ Lesson10_UIkit/Lesson10/Lesson10/NextViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NextViewController.swift
+//  Lesson10
+//
+//  Created by Марк Шнейдерман on 17.05.2021.
+//
+
+import UIKit
+
+final class NextViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
+
