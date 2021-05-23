ba02edc2aa42a483818ba24b7cec3c53086c527b
zardak12 <shmarkyr@gmail.com>
2021-05-23 20:36:29 +0300

    Update README.md

---
+++ Lesson13/TableView/TableView/CustomCell.swift
@@ -0,0 +1,8 @@
+//
+//  CustomCell.swift
+//  TableView
+//
+//  Created by Марк Шнейдерман on 25.05.2021.
+//
+
+import Foundation
---
+++ Lesson13/TableView/TableView/DetailViewContoller.swift
@@ -0,0 +1,8 @@
+//
+//  DetailViewContoller.swift
+//  TableView
+//
+//  Created by Марк Шнейдерман on 25.05.2021.
+//
+
+import Foundation
